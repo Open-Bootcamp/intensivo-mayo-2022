b40996a73a24150dbe60926c823611515f3e37e4
RaulDeImagina <raulmartos@imaginagroup.com>
2022-05-18 12:30:36 +0200

    Sesion 9

---
+++ Sesion 9/University/UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+
+        private readonly ILoggerFactory _loggerFactory;
+
+
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options, ILoggerFactory loggerFactory): base(options)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        // Add DbSets (Tables of our Data base)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Course>? Courses { get; set; }
+        public DbSet<Chapter>? Chapters { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Student>? Students { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var logger = _loggerFactory.CreateLogger<UniversityDBContext>();
+            // optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name }));
+            // optionsBuilder.EnableSensitiveDataLogging();
+
+            optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name }), LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
+    }
+}
+
+
